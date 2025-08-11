c68d5c7c97e84fa4cdc0f18bce2df0bd2c21d43b
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-08-11 18:59:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -0,0 +1,9 @@
+class Solution {
+    static boolean prime(int n) {
+        if (n <= 1) return false; // 0 and 1 are not prime
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false; // Found a divisor, not prime
+        }
+        return true; // No divisors found, prime
+    }
+}
