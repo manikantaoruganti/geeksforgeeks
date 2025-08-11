64910b02e4dff0405010f51427b7b80e5e079ff4
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-08-11 19:00:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/nextLine and next/next-line-and-next.java
@@ -0,0 +1,18 @@
+// Complete the function
+// Take input for a and s
+import java.util.*;
+
+class Geeks {
+    static void getInput() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Taking the number of testcases
+        while (t-- > 0) {
+            int a = sc.nextInt();  // reading integer
+            sc.nextLine();         // clearing the input buffer
+            String s = sc.nextLine(); // reading full line string
+            
+            System.out.println(a);
+            System.out.println(s);
+        }
+    }
+}
