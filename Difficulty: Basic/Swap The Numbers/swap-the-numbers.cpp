2963652eb5aea2eb0d45549ad4c0ad3cc0ea5429
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-08-11 19:05:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
@@ -0,0 +1,10 @@
+void swap(int &a, int &b) {
+    // Create a temporary variable to hold the value of 'a'
+    int temp = a;
+
+    // Assign the value of 'b' to 'a'
+    a = b;
+
+    // Assign the value of the temporary variable (which was 'a') to 'b'
+    b = temp;
+}
