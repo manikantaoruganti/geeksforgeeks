40aaf1011e3d27e8fce1453539df4ea9e0028eb6
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-02-20 11:31:21 +0530

    Clarify print statement in utility method
    Updated comment to clarify print statement functionality.

--- Difficulty_ Basic/Print With Space/print-with-space.java
+++ Difficulty_ Basic/Print With Space/print-with-space.java
@@ -3,7 +3,7 @@ public class Solution {
         String a = scn.next();
         String b = scn.next();
 
-        // Print 'a', followed by a space, then 'b', and finally a new line.
+        
         System.out.println(a + " " + b);
     }
 }
