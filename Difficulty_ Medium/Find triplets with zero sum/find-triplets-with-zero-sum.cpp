3aa1ea93fe9338e008d88b35ccb829714063193b
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-03-23 09:39:19 +0530

    Update find-triplets-with-zero-sum.cpp

--- Difficulty_ Medium/Find triplets with zero sum/find-triplets-with-zero-sum.cpp
+++ Difficulty_ Medium/Find triplets with zero sum/find-triplets-with-zero-sum.cpp
@@ -1,19 +1,15 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 class Solution {
   public:
- 
     bool findTriplets(vector<int> &arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
-
-        for (int i = 0; i < n - 2; i++) {
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
+       int n = arr.size();
+       sort(arr.begin(), arr.end());
+       for (int i = 0; i < n - 2; i++) {
+             int left = i + 1;
+             int right = n - 1;
+               while (left < right) {
                 int sum = arr[i] + arr[left] + arr[right];
  
                 if (sum == 0) {
