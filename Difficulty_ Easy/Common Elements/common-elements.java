1bfd90a9bac335e75fda8cf5bc7823d6da1616bc
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-04-19 09:23:37 +0530

    Update common-elements.java
    Removed comment about the two-pointer approach for clarity.

--- Difficulty_ Easy/Common Elements/common-elements.java
+++ Difficulty_ Easy/Common Elements/common-elements.java
@@ -9,7 +9,6 @@ class Solution {
         Arrays.sort(b);
         int i = 0, j = 0;
         
-        // Two-pointer approach to find common elements
         while (i < a.length && j < b.length) {
             if (a[i] == b[j]) {
                 result.add(a[i]);
