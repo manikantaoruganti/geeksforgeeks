ff080218749cc56847ad4b24e289b9e4cc9855d4
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-04-20 09:14:42 +0530

    Update chocolate-distribution-problem.py

--- Difficulty_ Easy/Chocolate Distribution Problem/chocolate-distribution-problem.py
+++ Difficulty_ Easy/Chocolate Distribution Problem/chocolate-distribution-problem.py
@@ -5,21 +5,21 @@ class Solution:
         # n = number of packets
         n = len(arr)
 
-        # if there are no students or no packets
+        
         if M == 0 or n == 0:
             return 0
 
-        # If students are more than packets
-        if n < M:
-            return -1   # not possible
 
-        # Sort the packets
+        if n < M:
+            return -1   
+
+        
         arr.sort()
  
-        # Initialize result as a large value
+    
         min_diff = float("inf")
 
-        # Find the subarray of size M with minimum difference
+    
         for i in range(n - M + 1):
             diff = arr[i + M - 1] - arr[i]
             if diff < min_diff:
